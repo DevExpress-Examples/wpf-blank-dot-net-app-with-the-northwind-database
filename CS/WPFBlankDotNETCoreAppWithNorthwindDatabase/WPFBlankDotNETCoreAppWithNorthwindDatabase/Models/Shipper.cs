f6dc62aa4a3c59868309fbb2313c588dfd044ac6
AndreySlabov <65009440+AndreySlabov@users.noreply.github.com>
2020-06-16 14:26:33 +0300

    Add files via upload

---
+++ CS/WPFBlankDotNETCoreAppWithNorthwindDatabase/WPFBlankDotNETCoreAppWithNorthwindDatabase/Models/Shipper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPFBlankDotNETCoreAppWithNorthwindDatabase.Models {
+    public partial class Shipper {
+        public Shipper() {
+            Orders = new HashSet<Order>();
+        }
+
+        public int ShipperId { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ CS/WPFBlankDotNETCoreAppWithNorthwindDatabase/WPFBlankDotNETCoreAppWithNorthwindDatabase/Models/Territory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPFBlankDotNETCoreAppWithNorthwindDatabase.Models {
+    public partial class Territory {
+        public string TerritoryId { get; set; }
+        public string TerritoryDescription { get; set; }
+        public int RegionId { get; set; }
+
+        public virtual Region Region { get; set; }
+    }
+}
