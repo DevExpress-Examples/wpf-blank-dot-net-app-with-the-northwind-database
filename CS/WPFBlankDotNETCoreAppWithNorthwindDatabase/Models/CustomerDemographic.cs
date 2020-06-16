6f4efe208c9d390b8f9f89334064293f18503cfa
Andrey Slabov <andrey.slabov@devexpress.com>
2020-06-16 14:58:13 +0300

    update

---
+++ CS/WPFBlankDotNETCoreAppWithNorthwindDatabase/Models/CustomerDemographic.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPFBlankDotNETCoreAppWithNorthwindDatabase.Models {
+    public partial class CustomerDemographic {
+        public string CustomerTypeId { get; set; }
+        public string CustomerDesc { get; set; }
+    }
+}
