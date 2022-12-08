f469aa930f464bd9ac90b6cb520ec72687431f7d
AndreySlabov <andrey.slabov@devexpress.com>
2022-12-08 18:25:52 +0400

    vb

--- VB/WPFBlankDotNETAppWithNorthwindDatabase/App.xaml.vb
+++ VB/WPFBlankDotNETAppWithNorthwindDatabase/App.xaml.vb
@@ -1,9 +1,11 @@
+Imports System.Windows
+
 Namespace WPFBlankDotNETAppWithNorthwindDatabase
 
     ''' <summary>
     ''' Interaction logic for App.xaml
     ''' </summary>
-    Public Partial Class App
+    Partial Public Class App
         Inherits Application
 
     End Class
